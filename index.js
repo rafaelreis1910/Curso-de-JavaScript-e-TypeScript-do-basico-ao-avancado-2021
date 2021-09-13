a67b3075a896b45426c3c74feb4861866ec00d65
Rafael Reis <80486202+rafaelreis1910@users.noreply.github.com>
2021-09-13 20:05:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+Operadores de comparação
+> maior que
+>= maior ou igual
+< menor que
+<= menor ou igual a 
+== igualdade (é atribuição)
+=== igualdade estrita (valor e tipo)
+!= diferente
+!== diferente estrito
+*/
+
+const comp2 = 20 > 10;
+console.log(comp2);
+
+const comp3 = 20 >= 20;
+console.log(comp3);
+
+const comp4 = 20 < 10;
+console.log(comp4);
+
+const comp5 = 20 <= 20;
+console.log(comp5);
+
+const comp6 = 20 === 20;
+console.log(comp6);
+
+const comp7 = 20 !== 20;
+console.log(comp7);
